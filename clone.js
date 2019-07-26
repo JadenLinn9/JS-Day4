d10eef5fe5f2e142c05fec3ad52354c463119d93
jadenlinn9 <jadenlinn9@gmail.com>
2019-07-26 10:52:43 +0800

    lesson

---
+++ clone.js
@@ -0,0 +1,14 @@
+function clone(source) {
+    if (null == source || typeof (source) != 'object') return null;
+
+    let target = new Object();
+    for (let attr in source) {
+        if (typeof (source[attr]) != 'object') {
+            target[attr] = source[attr];
+        } else {
+            target[attr] = clone(source[attr]);
+
+        }
+    }
+    return target;
+}
